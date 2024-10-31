861d0610e1876f6b4136e40d701c16a167b7da4a
Silvestre Fernandez <silvestref@missionreadyhq.com>
2024-10-31 16:51:10 +1300

    Commiting recent changes!

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
     public function homepage(): Response
     {
-        return new Response('Hello World!');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'starshipCount' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
