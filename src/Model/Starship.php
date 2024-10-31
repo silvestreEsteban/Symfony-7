ad92bc7732e02d5ae1464b0e890fa0c06129960f
Silvestre Fernandez <silvestref@missionreadyhq.com>
2024-11-01 12:15:36 +1300

    Added some templates for the styling. Made the content shown on the screen dynamic. Created an Enum, called the values through a Getter method on to the page with twig.

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -9,7 +9,7 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status,
     )
     {
 
@@ -35,11 +35,21 @@ class Starship
         return $this->captain;
     }
 
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
 
+    public function getStatusString(): string
+    {
+        return $this->status->value;
+    }
 
-
+    public function getStatusImageFilename(): string{
+        return match($this->status) {
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+        };
+    }
 }
